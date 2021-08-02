975e1d9b5b98a26f303d407d2a268a881ce315bd
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2021-08-02 17:19:13 +0700

    Creating the Activities API Controller

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,26 @@
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await _mediator.Send(new List.Query());
+        }
+    }
+}
