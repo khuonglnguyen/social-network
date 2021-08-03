b6a727cbaf664ab7a3f256d355d937778d422f03
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2021-08-03 12:58:31 +0700

    Adding an Activity interface in Typescript

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-import "./App.css";
 import axios from "axios";
+import { IActivity } from "../models/activity";
 
-class App extends Component {
-  state = {
+interface IState{
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+  readonly state:IState = {
     activities: [],
   };
 
   componentDidMount() {
-    axios.get("https://localhost:5001/api/activities").then((response) => {
-      console.log(response);
+    axios.get<IActivity[]>("https://localhost:5001/api/activities").then((response) => {
       this.setState({
         activities: response.data,
       });
@@ -26,7 +29,7 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.activities.map((activity: any) => (
+          {this.state.activities.map((activity) => (
             <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
