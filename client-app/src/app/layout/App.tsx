5dad83e2f32f2d83ababa00414411f95c5074549
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2021-08-06 13:32:43 +0700

    Styling React components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Header, Icon, List } from "semantic-ui-react";
+import React, { useState, useEffect, Fragment } from "react";
+import { Container, Header, Icon, List } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 import { IActivity } from "../models/activity";
@@ -17,15 +17,17 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <Fragment>
       <NavBar></NavBar>
+      <Container style={{marginTop: '7em'}}>
       <List>
         {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
       </List>
+      </Container>
       <ul></ul>
-    </div>
+    </Fragment>
   );
 };
 
