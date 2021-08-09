792c9f3facd48369f13366c29b9166abe2c5a285
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2021-08-10 06:36:55 +0700

    Listing our activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-import axios from "axios";
 import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -43,11 +43,10 @@ const App = () => {
   };
 
   useEffect(() => {
-    axios
-      .get<IActivity[]>("https://localhost:5001/api/activities")
+    agent.Activities.list()
       .then((response) => {
         let activities: IActivity[] = [];
-        response.data.forEach((activity) => {
+        response.forEach((activity) => {
           activity.date = activity.date.split(".")[0];
           activities.push(activity);
         });
